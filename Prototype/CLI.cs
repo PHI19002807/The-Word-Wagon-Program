5680bc48f96d28f8562f855072d908755de6eb6b
gailp <gailp@GAMER>
2023-01-13 16:21:55 +0000

    Add project files.

---
+++ Prototype/CLI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    internal static class CLI
+    {
+        public static Library library { get; set; }
+        public static void MainMenu()
+        {
+
+        }
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LIBRARY
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CLI.library = new library("The Word Wagon", "123 Street, Taunton");
+            CLI.MainMenu();
+
+        }
+    }
+
+
+}
