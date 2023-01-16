692e4c93fd887ae86eb230fa3d7bd348f3f76124
William Phillips <PHI19002807@btc.ac.uk>
2023-01-16 11:40:54 +0000

    items 1

--- Prototype/Items.cs
+++ Prototype/Items.cs
@@ -1,13 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Prototype
+﻿namespace Prototype
 {
     internal class Items
     {
+        public string Title { get; set; } //Auto-gen Properties
+        public string Author { get; set; }
+        public string Id { get; set; }
+        public Vehicle(string title, 
+                       string author, 
+                       int id)
+        {
+            Title = title;
+            Author = author;
+            Id = id;
+        }
+
+        public override string? ToString()
+        {
+            return $"Title: { Title } \nAuthor: { Author } \nID: { Id } ";
+        }
 
     }
 }
