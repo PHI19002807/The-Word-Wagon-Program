83f1e49eff1adbd890dc05d89712343c0d46e18e
William Phillips <PHI19002807@btc.ac.uk>
2023-01-17 10:27:18 +0000

    3

--- Prototype/Loan.cs
+++ Prototype/Loan.cs
@@ -13,19 +13,21 @@ namespace Encapsulation
         public bool Status { get; set; }
         public string ReturnDate { get; set; }
         public string CustomerInfo { get; set; }
+        public Items Item { get; set; }
 
-        public Loan(DateTime currentdate, string duration, bool status, string returndate, string customerinfo)
+        public Loan(DateTime currentdate, string duration, bool status, string returndate, string customerinfo, Items items)
         {
             CurrentDate = currentdate;
             Duration = duration;
             Status = status;
             ReturnDate = returndate;
             CustomerInfo = customerinfo;
+            Item = items;
         }
 
         public override string ToString()
         {
-            return $"";
+            return $"Date: { CurrentDate }\nDuration: { Duration }\nStatus: { Status }\nReturn date: { ReturnDate }\nCustomer info: { CustomerInfo }";
         }
     }
 }
